d102db0a4abb728a9b71b49965cf5bb22570774b
Momin Juned <68558066+mjuned47@users.noreply.github.com>
2025-04-03 12:57:10 +0530

    install pyparsing module

--- packages/libspice-server/build.sh
+++ packages/libspice-server/build.sh
@@ -14,6 +14,7 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_pre_configure() {
+	pip install pyparsing 
 	rm -f "$TERMUX_PKG_SRCDIR"/configure
 	LDFLAGS+=" $($CC -print-libgcc-file-name) -landroid-glob -lc++_shared"
 }
