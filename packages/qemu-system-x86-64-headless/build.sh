2467d06670df12e1e39ae4a8366d6d24581bba89
mjuned47 <junedmomin574@gmail.com>
2025-05-08 20:43:58 +0530

    Build QEMU-4.2

--- packages/qemu-system-x86-64-headless/build.sh
+++ packages/qemu-system-x86-64-headless/build.sh
@@ -1,60 +1,27 @@
 TERMUX_PKG_HOMEPAGE=https://www.qemu.org
 TERMUX_PKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (headless)"
-TERMUX_PKG_LICENSE="GPL-2.0"
-TERMUX_PKG_MAINTAINER="@termux"
-TERMUX_PKG_VERSION=1:8.2.6
-TERMUX_PKG_REVISION=6
-TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${TERMUX_PKG_VERSION:2}.tar.xz
-TERMUX_PKG_SHA256=8cadb1e6b039954e672d4a7cc3a5f30738b4cb99bc92c2640b15cc89f8f91fa2
-TERMUX_PKG_DEPENDS="alsa-lib, dtc, glib, jack2, libbz2, libcurl, libdw, libgmp, libgnutls, libiconv, libjpeg-turbo, liblzo, libnettle, libnfs, libpixman, libpng, libslirp, libspice-server, libssh, libusb, libusbredir, ncurses, pulseaudio, qemu-common, resolv-conf, zlib, zstd"
-
-# Required by configuration script, but I can't find any binary that uses it.
-TERMUX_PKG_BUILD_DEPENDS="libtasn1"
-
-TERMUX_PKG_CONFLICTS="qemu-system-x86_64-headless"
-TERMUX_PKG_REPLACES="qemu-system-x86_64-headless"
-TERMUX_PKG_PROVIDES="qemu-system-x86_64-headless"
+TERMUX_PKG_LICENSE="LGPL-2.1"
+TERMUX_PKG_MAINTAINER="Leonid Pliushch <leonid.pliushch@gmail.com>"
+# Do not update version unless you verified that it works properly.
+_PACKAGE_VERSION=4.2.1
+TERMUX_PKG_VERSION=1:${_PACKAGE_VERSION}
+TERMUX_PKG_REVISION=4
+TERMUX_PKG_SRCURL=https://download.qemu.org/qemu-${_PACKAGE_VERSION}.tar.xz
+TERMUX_PKG_SHA256="7e331163b72e7bcf63bd35cb85cba87b48d12fab3f264b94c23f7d3991094207"
+TERMUX_PKG_DEPENDS="attr, glib, libbz2, libc++, libcap, libcurl, libgcrypt, libiconv, libjpeg-turbo, liblzo, libpixman, libpng, libssh, ncurses, qemu-common, resolv-conf, zlib, libusbredir, libspice-server"
+#TERMUX_PKG_CONFLICTS="qemu-system-x86_64-headless"
+#TERMUX_PKG_REPLACES="qemu-system-x86_64-headless"
+TERMUX_PKG_PROVIDES="qemu-system-x86_64-headless-${_PACKAGE_VERSION}"
 TERMUX_PKG_BUILD_IN_SRC=true
 
-termux_step_pre_configure() {
-	# Workaround for https://github.com/termux/termux-packages/issues/12261.
-	if [ $TERMUX_ARCH = "aarch64" ]; then
-		rm -f $TERMUX_PKG_BUILDDIR/_lib
-		mkdir -p $TERMUX_PKG_BUILDDIR/_lib
-
-		cd $TERMUX_PKG_BUILDDIR
-		mkdir -p _setjmp-aarch64
-		pushd _setjmp-aarch64
-		mkdir -p private
-		local s
-		for s in $TERMUX_PKG_BUILDER_DIR/setjmp-aarch64/{setjmp.S,private-*.h}; do
-			local f=$(basename ${s})
-			cp ${s} ./${f/-//}
-		done
-		$CC $CFLAGS $CPPFLAGS -I. setjmp.S -c
-		$AR cru $TERMUX_PKG_BUILDDIR/_lib/libandroid-setjmp.a setjmp.o
-		popd
-
-		LDFLAGS+=" -L$TERMUX_PKG_BUILDDIR/_lib -l:libandroid-setjmp.a"
-	fi
-}
-
 termux_step_configure() {
-	termux_setup_ninja
-
-	if [ "$TERMUX_ARCH" = "i686" ]; then
-		LDFLAGS+=" -latomic"
-	fi
-
 	local QEMU_TARGETS=""
+	export QEMU_PREFIX="${TERMUX_PREFIX}\qemu-${_PACKAGE_VERSION}"
 
 	# System emulation.
 	QEMU_TARGETS+="aarch64-softmmu,"
 	QEMU_TARGETS+="arm-softmmu,"
 	QEMU_TARGETS+="i386-softmmu,"
-	QEMU_TARGETS+="m68k-softmmu,"
-	QEMU_TARGETS+="ppc64-softmmu,"
-	QEMU_TARGETS+="ppc-softmmu,"
 	QEMU_TARGETS+="riscv32-softmmu,"
 	QEMU_TARGETS+="riscv64-softmmu,"
 	QEMU_TARGETS+="x86_64-softmmu,"
@@ -63,9 +30,6 @@ termux_step_configure() {
 	QEMU_TARGETS+="aarch64-linux-user,"
 	QEMU_TARGETS+="arm-linux-user,"
 	QEMU_TARGETS+="i386-linux-user,"
-	QEMU_TARGETS+="m68k-linux-user,"
-	QEMU_TARGETS+="ppc64-linux-user,"
-	QEMU_TARGETS+="ppc-linux-user,"
 	QEMU_TARGETS+="riscv32-linux-user,"
 	QEMU_TARGETS+="riscv64-linux-user,"
 	QEMU_TARGETS+="x86_64-linux-user"
@@ -74,11 +38,15 @@ termux_step_configure() {
 	CXXFLAGS+=" $CPPFLAGS"
 	LDFLAGS+=" -landroid-shmem -llog"
 
+	cp "$TERMUX_PREFIX"/bin/libgcrypt-config \
+		"$TERMUX_PKG_TMPDIR"/libgcrypt-config
+	export PATH="$PATH:$TERMUX_PKG_TMPDIR"
+
 	# Note: using --disable-stack-protector since stack protector
 	# flags already passed by build scripts but we do not want to
 	# override them with what QEMU configure provides.
 	./configure \
-		--prefix="$TERMUX_PREFIX" \
+		--prefix="$QEMU_PREFIX" \
 		--cross-prefix="${TERMUX_HOST_PLATFORM}-" \
 		--host-cc="gcc" \
 		--cc="$CC" \
@@ -87,11 +55,11 @@ termux_step_configure() {
 		--disable-stack-protector \
 		--smbd="$TERMUX_PREFIX/bin/smbd" \
 		--enable-coroutine-pool \
-		--audio-drv-list=pa \
 		--enable-trace-backends=nop \
 		--disable-guest-agent \
-		--enable-gnutls \
-		--enable-nettle \
+		--disable-gnutls \
+		--disable-nettle \
+		--enable-gcrypt \
 		--disable-sdl \
 		--disable-sdl-image \
 		--disable-gtk \
@@ -101,41 +69,41 @@ termux_step_configure() {
 		--enable-vnc \
 		--disable-vnc-sasl \
 		--enable-vnc-jpeg \
-		--enable-png \
+		--enable-vnc-png \
 		--disable-xen \
 		--disable-xen-pci-passthrough \
 		--enable-virtfs \
 		--enable-curl \
-		--enable-fdt=system \
+		--enable-fdt \
 		--enable-kvm \
+		--disable-hax \
 		--disable-hvf \
 		--disable-whpx \
 		--enable-libnfs \
+		--enable-libusb \
 		--enable-lzo \
 		--disable-snappy \
+		--enable-spice \
 		--enable-bzip2 \
 		--disable-lzfse \
 		--disable-seccomp \
 		--enable-libssh \
+		--enable-libxml2 \
 		--enable-bochs \
 		--enable-cloop \
 		--enable-dmg \
 		--enable-parallels \
 		--enable-qed \
-		--enable-slirp \
-		--enable-spice \
-		--enable-libusb \
-		--enable-usb-redir \
-		--disable-vhost-user \
-		--disable-vhost-user-blk-server \
+		--enable-sheepdog \
 		--target-list="$QEMU_TARGETS"
 }
 
 termux_step_post_make_install() {
 	local i
-	for i in aarch64 arm i386 m68k ppc ppc64 riscv32 riscv64 x86_64; do
+	for i in aarch64 arm i386 riscv32 riscv64 x86_64; do
 		ln -sfr \
 			"${TERMUX_PREFIX}"/share/man/man1/qemu.1 \
 			"${TERMUX_PREFIX}"/share/man/man1/qemu-system-${i}.1
+		ln -sfr $QEMU_PREFIX/bin/qemu-system-${i} "${TERMUX_PREFIX}"/bin/qemu-system-${i}-${_PACKAGE_VERSION}
 	done
 }
--- packages/qemu-system-x86-64-headless/qemu-system-riscv32-headless.subpackage.sh
+++ packages/qemu-system-x86-64-headless/qemu-system-riscv32-headless.subpackage.sh
@@ -1,5 +1,6 @@
 TERMUX_SUBPKG_DESCRIPTION="A generic and open source machine emulator and virtualizer (headless)"
-TERMUX_SUBPKG_DEPEND_ON_PARENT=deps
+TERMUX_SUBPKG_DEPENDS="attr, glib, libbz2, libc++, libcap, libcurl, libgcrypt, libiconv, libjpeg-turbo, liblzo, libpixman, libpng, libssh, ncurses, qemu-common, resolv-conf, zlib"
+TERMUX_SUBPKG_DEPEND_ON_PARENT=no
 
 TERMUX_SUBPKG_INCLUDE="
 bin/qemu-system-riscv32
